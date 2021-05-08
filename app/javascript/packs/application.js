8b2c3346a1880991f678e273303e227a6405df6e
David Blackwelder <github@davidblackwelder.com>
2021-05-07 21:42:07 -0400

    Added glightbox

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,8 +10,9 @@ import "channels";
 
 import "bootstrap/dist/js/bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "boxicons/css/boxicons.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
+import "boxicons/css/boxicons.min.css";
+import Glightbox from "glightbox";
 
 import "stylesheets/style";
 import "../src/main";
